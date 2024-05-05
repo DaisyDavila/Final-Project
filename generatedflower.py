35b06e8a910a92002f39b08990b68df2a27ee85d
Daisy Davila <daisy03davila@gmail.com>
2024-05-05 18:27:17 -0500

    I was able to draw a flower.

--- generatedflower.py
+++ generatedflower.py
@@ -12,3 +12,22 @@ def draw_flower(t, petals, radius, angle):
     for _ in range(petals):
         draw_petal(t, radius, angle)
         t.left(360 / petals)
+
+def main():
+    # adding color and how petal information
+    screen = turtle.Screen()
+    screen.bgcolor((173/255, 230/255, 230/255))  # Light blue background color in RGB!
+
+    petal_turtle = turtle.Turtle()
+    petal_turtle.color((1, 1, 1))  # white color in RGB !
+    petal_turtle.speed(0)  # Setting the speed to the fastest to be able to see the flower faster
+
+    # Draw a flower
+    draw_flower(petal_turtle, petals=12, radius=100, angle=45)
+
+    # Hiding the little turtle 
+    petal_turtle.hideturtle()
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
