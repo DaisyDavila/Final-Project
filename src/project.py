ade7bec5969a755e6497f74b2e94956ae5dd0e50
Daisy Davila <daisy03davila@gmail.com>
2024-05-05 21:43:42 -0500

    I added where you can click the screen and place a yellow dot, I want that to be the bud of the flower.

--- src/project.py
+++ src/project.py
@@ -1,51 +1,104 @@
 import turtle
 
 def draw_petal(t, radius, angle):
-    """Draw a single petal"""
+    """Draw a single petal."""
+    t.begin_fill()
     t.circle(radius, angle)
     t.left(180 - angle)
     t.circle(radius, angle)
     t.left(180 - angle)
+    t.end_fill()
 
 def draw_flower(t, petals, radius, angle):
-    """Drawing the flower petals"""
+    """Draw a flower with a specified number of petals."""
     for _ in range(petals):
         draw_petal(t, radius, angle)
         t.left(360 / petals)
 
 def draw_stem(t, length):
-    """Draw a stem below the flower"""
-    t.left(90)  # Turn left to draw stem
+    """Draw a stem below the flower."""
+    t.left(90)  # Turn to the left to draw stem
     t.forward(length)  # Draw stem
+
+def click(x, y):
+    """Turtle for clicking"""
+    fill_turtle.penup()
+    fill_turtle.goto(x, y)
+    fill_turtle.pendown()
+    fill_turtle.dot(30, (255/255, 234/255, 13/255))  # Yellow Bud color
+
 
 def main():
     # Set up the turtle
     screen = turtle.Screen()
     screen.bgcolor((173/255, 230/255, 230/255))  # Light blue background color in RGB!
 
-    petal_turtle = turtle.Turtle()
-    petal_turtle.color((1, 1, 1))  # Outline of the colors are White!
-    petal_turtle.speed(0)  # Setting the speed to the fastest to be able to see the flower faster
+    # Draw the flower in the center
+    petal_turtle_center = turtle.Turtle()
+    petal_turtle_center.color("white")  # White Daisy Petals
+    petal_turtle_center.speed(0)  # Setting the speed to the fastest to be able to see the flower faster
+    petal_turtle_center.begin_fill()  # Begin filling petals
+    draw_flower(petal_turtle_center, petals=11, radius=100, angle=36)  # Draw the centerflower
+    petal_turtle_center.end_fill()  # End filling petals
 
-    # Draw a flower
-    draw_flower(petal_turtle, petals=11, radius=100, angle=45)
+    # Draw the flower on the left
+    petal_turtle_left = turtle.Turtle()
+    petal_turtle_left.color("white")  # White Daisy Petals
+    petal_turtle_left.speed(0)
+    petal_turtle_left.penup()
+    petal_turtle_left.goto(-200, 0)  
+    petal_turtle_left.pendown()
+    petal_turtle_left.begin_fill()
+    draw_flower(petal_turtle_left, petals=11, radius=100, angle=36)
+    petal_turtle_left.end_fill()
 
-    # Make the stem touch the center of the petals
-    stem_length = 300  # Adjust this value as needed
+    # Draw the flower on the right
+    petal_turtle_right = turtle.Turtle()
+    petal_turtle_right.color("white")  # White Daisy Petals
+    petal_turtle_right.speed(0)
+    petal_turtle_right.penup()
+    petal_turtle_right.goto(200, 0)  
+    petal_turtle_right.pendown()
+    petal_turtle_right.begin_fill()
+    draw_flower(petal_turtle_right, petals=11, radius=100, angle=36)
+    petal_turtle_right.end_fill()
 
-    # Turtle for drawing stem
-    stem_turtle = turtle.Turtle()
+    # Calculate the stem length to touch the center of the petals
+    stem_length = 120  
+    # Draw the stem for the central flower
+    stem_turtle_center = turtle.Turtle()
+    stem_turtle_center.penup()
+    stem_turtle_center.goto(0, -stem_length)  
+    stem_turtle_center.pendown()
+    stem_turtle_center.color((50/255, 205/255, 50/255))
+    draw_stem(stem_turtle_center, length=stem_length)
 
-    # Draw a stem below the flower
-    stem_turtle.penup()
-    stem_turtle.goto(0, -stem_length)  # Move turtle below the flower
-    stem_turtle.pendown()
-    stem_turtle.color((50/255, 205/255, 50/255))
-    draw_stem(stem_turtle, length=stem_length)
+    # Draw the stem for the flower on the left
+    stem_turtle_left = turtle.Turtle()
+    stem_turtle_left.penup()
+    stem_turtle_left.goto(-200, -stem_length)  
+    stem_turtle_left.pendown()
+    stem_turtle_left.color((50/255, 205/255, 50/255))
+    draw_stem(stem_turtle_left, length=stem_length)
 
-    # Hiding the little turtle 
-    petal_turtle.hideturtle()
-    stem_turtle.hideturtle()
+    # Draw the stem for the flower on the right
+    stem_turtle_right = turtle.Turtle()
+    stem_turtle_right.penup()
+    stem_turtle_right.goto(200, -stem_length)  
+    stem_turtle_right.pendown()
+    stem_turtle_right.color((50/255, 205/255, 50/255))
+    draw_stem(stem_turtle_right, length=stem_length)
+
+    # A turtle for placing the yellow bud on flowers!
+    global fill_turtle
+    fill_turtle = turtle.Turtle()
+    fill_turtle.hideturtle()
+    fill_turtle.speed(0)
+    fill_turtle.penup()
+
+    # Bind click event to add the yellow bud
+    screen.onclick(click)
+
     screen.mainloop()
 
 if __name__ == "__main__":
